c901fcaba9a060b1c8cd4156d32c46923eb3746f
captain-brown <myc0059@gmail.com>
2022-05-25 21:18:35 +0900

    remove unnecessary env keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,14 +16,29 @@ const config: HardhatUserConfig = {
       },
     },
     baobab: {
-      url: process.env.BAOBAB_URL,
+      url: 'https://node-api.klaytnapi.com/v1/klaytn',
       httpHeaders: {
         Authorization:
           'Basic ' +
           Buffer.from(
             process.env.KAS_ACCESS_KEY + ':' + process.env.KAS_SECRET_KEY,
           ).toString('base64'),
-        'x-chain-id': process.env.CHAIN_ID || '1001',
+        'x-chain-id': '1001',
+      },
+      accounts: [process.env.PRIVATEKEY || ''],
+      chainId: 1001,
+      gas: 8500000,
+      gasPrice: 250000000000,
+    },
+    cypress: {
+      url: 'https://node-api.klaytnapi.com/v1/klaytn',
+      httpHeaders: {
+        Authorization:
+          'Basic ' +
+          Buffer.from(
+            process.env.KAS_ACCESS_KEY + ':' + process.env.KAS_SECRET_KEY,
+          ).toString('base64'),
+        'x-chain-id': '8217',
       },
       accounts: [process.env.PRIVATEKEY || ''],
       chainId: 1001,
